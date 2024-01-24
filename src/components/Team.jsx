3084f6d5062584d2f842f9ff548b0c8303695f90
milton <miltonelim64@gmail.com>
2024-01-24 13:57:56 +0300

    maamama

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -5,10 +5,9 @@ export const Team = (props) => {
     <div id="team" className="text-center">
       <div className="container">
         <div className="col-md-8 col-md-offset-2 section-title">
-          <h2>Meet the Team</h2>
+          <h2>Where to Find US</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+          The geographical spread allows us to seamlessly connect with our clients, providing personalized and efficient services tailored to your unique needs.
           </p>
         </div>
         <div id="row">
--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="#page-top">
-            React Landing Page
+            Jadzcotech Solutions
           </a>{" "}
         </div>
 
@@ -29,7 +29,7 @@ export const Navigation = (props) => {
           <ul className="nav navbar-nav navbar-right">
             <li>
               <a href="#features" className="page-scroll">
-                Features
+                Products
               </a>
             </li>
             <li>
@@ -47,14 +47,14 @@ export const Navigation = (props) => {
                 Gallery
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href="#testimonials" className="page-scroll">
                 Testimonials
               </a>
-            </li>
+            </li> */}
             <li>
               <a href="#team" className="page-scroll">
-                Team
+                Find Us
               </a>
             </li>
             <li>
